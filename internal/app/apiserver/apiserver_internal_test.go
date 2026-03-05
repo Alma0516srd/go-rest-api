e44429ea10405b6db39ffccbd522948f510a1888
a.manukhin <aleksander.manukhin@gmail.com>
2026-03-05 10:00:08 +0500

    tests

--- internal/app/apiserver/apiserver_internal_test.go
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -1,1 +1,17 @@
 package apiserver
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApiServer_HandleHello(t *testing.T) {
+	apiserver := New(NewConfig())
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodGet, "/hello", nil)
+	apiserver.handleHello().ServeHTTP(recorder, request)
+	assert.Equal(t, "Hello World", recorder.Body.String())
+}
